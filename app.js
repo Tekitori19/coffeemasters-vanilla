9740339991a591e1efa7c7aed88b8f4430248245
Dwcks Dinh <corclan19@gmail.com>
2023-09-03 09:07:17 +0700

    add app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded",() => {
+    let nav = document.querySelector("nav");
+    console.log(nav);
+    // nav.innerHTML =`
+    //     <h1>This is nav</h1>
+    //     <p>This is param</p>
+    // `;
+})
